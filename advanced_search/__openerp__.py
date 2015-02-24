a6346ef18d628268db403bfc8bc0cc87ae9dd9c7
Nehal <nehal@pm.biztechconsultancy.com>
2015-02-24 16:55:14 +0530

    Changed Price

--- advanced_search/__openerp__.py
+++ advanced_search/__openerp__.py
@@ -18,7 +18,7 @@ Supplier Invoice, Supplier Refund, Purchase Orders, Purchase Receipts.
               ],
     'demo': [],
     'installable': True,
-    'price': 9.9,
+    'price': 1.0,
     'currency': 'USD',
     'auto_install': False,
 }
